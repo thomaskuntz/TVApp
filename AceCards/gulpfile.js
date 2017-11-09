2b8ccf99fa3e83162eb7ba11916b4cefc8e2bd7b
Christopher Valladares <vallchri10@gmail.com>
2017-11-09 11:06:41 -0600

    Implementing New Features

---
+++ AceCards/gulpfile.js
@@ -0,0 +1,24 @@
+﻿/// <binding BeforeBuild='copy-assets' />
+"use strict";
+
+var _ = require('lodash'),
+    gulp = require('gulp');
+
+gulp.task('copy-assets', function () {
+    var assets = {
+        js: [
+            './node_modules/bootstrap/dist/js/bootstrap.js',
+            './node_modules/systemjs/dist/system.src.js',
+            './node_modules/angular2/bundles/angular2.dev.js',
+            './node_modules/angular2/bundles/router.dev.js',
+            './node_modules/angular2/bundles/angular2-polyfills.js',
+            './node_modules/angular2/bundles/http.dev.js',
+            './node_modules/rxjs/bundles/Rx.js',
+            './node_modules/typescript/lib/typescript.js'
+        ],
+        css: ['./node_modules/bootstrap/dist/css/bootstrap.css']
+    };
+    _(assets).forEach(function (assets, type) {
+        gulp.src(assets).pipe(gulp.dest('./wwwroot/' + type));
+    });
+});
