0a3822288c7b601492afc85a7301713c58c63fe8
Christopher Valladares <vallchri10@gmail.com>
2018-01-12 13:48:27 -0600

    Mass Enhancement
    * Update Angular 1.5.7 to 1.6.8
    * Added Error Controlller + View (Future Enhancement)
    * Added AnuglarRouting (Future Enchancement)
    * Removed Scoreboard Model
    * Removed gaugeSetting (now being controlled through angular)
    * Enhanced card view + Gauge (gauge will show different color depending on the status)
    * Gauge will now update (NO TIMER!) if there is data change
    * Page Footer now shows date and online status

---
+++ AceCards/wwwroot/js/app.js
@@ -0,0 +1,11 @@
+﻿var app = angular.module('appRoute', ['ngRoute']);
+
+app.config([('$routeProvider')],
+    function ($routeProvider) {
+        $routeProvider
+            .when('/Error', {
+                templateUrl: 'Error/Error',
+                controller: 'ErrorController'
+            })
+            .otherwise({ redirectTo: "/Home" });
+    });
